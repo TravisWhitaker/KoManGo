1cc3e252136540a61b771763bbcbd6cff7456794
Julian Hammerstein <jjhammerstein@gmail.com>
2013-02-16 18:39:51 -0500

    More board stuff

--- src/board.c
+++ src/board.c
@@ -8,9 +8,21 @@
 ○ WHITE
 ─ DASH
 ★ STAR
-
-
 */
+
+wchar_t TL = "\u250C";
+wchar_t T = "\u252C";
+wchar_t TR = "\u2510";
+wchar_t L = "\u251C";
+wchar_t M = "\u253C";
+wchar_t R = "\u2524";
+wchar_t BL = "\u2514";
+wchar_t B = "\u2534";
+wchar_t BR = "\u2518";
+wchar_t D = "\u2500";
+wchar_t BLACK = "\u25CF";
+wchar_t WHITE = "\u25CB";
+wchar_t STAR = "\u2605";
 
 char *initBoard(int DIM)
 {
@@ -18,46 +30,31 @@ char *initBoard(int DIM)
 	for(r = 0; r < DIM; r++) //Everything is intersections
 	{
 		for(c = 0; c < DIM; c++)	
-			board[i][j] = 0;
+			board[r][c] = M;
 	}
 
 	for(c = 0; c < DIM; c++) 
 	{
-		board[0][c] = 2;		// Top tiles
-		board[DIM-1][c] = 8;	//Bottom tiles
+		board[0][c] = T;		// Top tiles
+		board[DIM-1][c] = B;	//Bottom tiles
 	}
 
 	for(r = 0; c < DIM; c++)
 	{
-		board[r][0] = 4; 	//Left tiles
-		board[r][DIM-1] = 6; //Right tiles
+		board[r][0] = L; 	//Left tiles
+		board[r][DIM-1] = R; //Right tiles
 	}
 
-	board[0][0] = 1; 		//Top Left corner
-	board[0][DIM-1] = 3; 	//Top right corner
-	board[DIM-1][0] = 7; 	//Bottom left corner
-	board[DIM-1][DIM-1] = 9; //Bottom right corner
+	board[0][0] = TL; 		//Top Left corner
+	board[0][DIM-1] = TR; 	//Top right corner
+	board[DIM-1][0] = BL; 	//Bottom left corner
+	board[DIM-1][DIM-1] = BR; //Bottom right corner
 
 	return board;
 }
 
 wchar_t *printBoard(char *board)
 {
-	wchar_t TL = "\u250C";
-	wchar_t T = "\u252C";
-	wchar_t TR = "\u2510";
-	wchar_t L = "\u251C";
-	wchar_t M = "\u253C";
-	wchar_t R = "\u2524";
-	wchar_t BL = "\u2514";
-	wchar_t B = "\u2534";
-	wchar_t BR = "\u2518";
-	wchar_t D = "\u2500";
-	wchar_t BLACK = "\u25CF";
-	wchar_t WHITE = "\u25CB";
-	wchar_t STAR = "\u2605";
-
-
 	wchar_t returnArray[DIM][2*DIM-1];
 	for(r = 0; r < DIM; r++)
 	{
